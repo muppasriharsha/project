02350a708481fd83eac4822b8d907b65eb3c183b
muppasriharsha <appstekharsha@gmail.com>
2021-04-15 10:58:51 +0530

    new_project is uploaded

---
+++ src/data.js
@@ -0,0 +1,89 @@
+const data = {
+
+    products:[
+        
+        {
+            _id:'1',
+            name:'samsung s10',
+            category:'electronies',
+            image:'/images/mobile.jpg',
+            price: 3000,
+            brand: 'samsung',
+            rating:4.5,
+            numReviews: 10,
+            description: 'high quality production'
+
+        },
+        //2 product
+        {
+            _id:'2',
+            name:'tablet',
+            category:'electronies',
+            image:'/images/tab.png',
+            price: 9000,
+            brand: 'i ball 10',
+            rating:4,
+            numReviews: 80,
+            description: 'high quality production'
+
+        },
+        //3
+        {
+            _id:'3',
+            name:'laptop',
+            category:'electronies',
+            image:'/images/laptop.jpg',
+            price: 55000,
+            brand: 'dell',
+            rating:5,
+            numReviews: 190,
+            description: 'high quality production'
+
+        },
+        //4
+        {
+            _id:'4',
+            name:'royal enfield',
+            category:'moter',
+            image:'/images/bullet.png',
+            price: 199000,
+            brand: 'royal enfield',
+            rating:3.5,
+            numReviews: 10,
+            description: 'high quality pants'
+
+        },
+        //5
+        {
+            _id:'5',
+            name:'shoes',
+            category:'accesiores',
+            image:'/images/shoes.png',
+            price: 3999,
+            brand: 'wood land',
+            rating:4.5,
+            numReviews: 10,
+            description: 'high quality production'
+
+        },
+        //6
+        {
+            _id:'6',
+            name:'air pots',
+            category:'electronies',
+            image:'/images/air pots.jpg',
+            price: 5000,
+            brand: 'apple',
+            rating:4,
+            numReviews: 10,
+            description: 'high quality pants'
+
+        }
+
+    ]
+
+
+
+}
+
+export default data;
